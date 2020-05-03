c60abd9b4dc4bb8bf4c442b2f1a4b620b39cca5e
Yevych <bohdan.yevych@gmail.com>
2020-05-03 17:00:01 +0300

    SEO

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -39,8 +39,13 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
 import { required } from 'vuelidate/lib/validators'
-
+import localeFilter from '@/filters/localize.filter'
 export default {
+  metaInfo() {
+    return {
+      title: this.$title('ProfileTitle')
+    }
+  },
   data: () => ({
     name: '',
     isRuLocale: true
@@ -65,6 +70,7 @@ export default {
         this.$v.$touch()
         return
       }
+      
       try {
         await this.updateInfo({
           name: this.name,
